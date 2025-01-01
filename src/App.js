edab31a3e7243fac8bebd9ebbc7e6e4946349d43
Shams Ur Rahman <shamsurrahman1578@gmail.com>
2025-01-01 07:20:01 -0800

    didUpdate

--- src/App.js
+++ src/App.js
@@ -6,24 +6,25 @@ import './App.css';
 constructor()
 {
   super();
-  // console.warn("Constructore (Must Call First)")
-  this.state={name:"Sir Hammad"}
+  console.warn("Constructore (Must Call First)")
+  this.state={count:0}
 }
-componentDidMount()
+componentDidUpdate(preProps,preState,snapShot)
 {
-  console.warn("Componentdidmount (Must Call at Last)")
+  console.warn("Component Did_Update (Will Update after defininng of props or state)",preState,preProps,snapShot)
+  if(this.state.count<10)
+  {
+    this.setState({count:this.state.count+1})
+  }
 }
 render()
 {
   console.warn("Render (Must Call Second)")
-  // const[name,setName]=React.useState("Hammad Khan")
   return(
     <div className="App">
-    <h1 style={{color:"red"}}>Component Did_Mount {this.state.name}</h1>
-    <p>In case of state componentDidMount will Not work while Render will work</p>
-    <button style={{borderRadius:"30%",color:"blue",height:"40px",fontFamily:"impact",fontSize:"20px",width:"150px"}} onClick={()=>this.setState({name:"Sir Hammad Khan"})}>Update Name</button>
-    {/* <Render /> */}
-    {/* <button onClick={()=>setName("Sir Hammad Khan")}>update teacher</button> */}
+    <h1 style={{color:"brown"}}>Component Did_Update {this.state.count}</h1>
+    <p>Will update after defining of props or states </p>
+    { <button style={{borderRadius:"30%",color:"blue",height:"40px",fontFamily:"impact",fontSize:"20px",width:"150px"}} onClick={()=>this.setState({count:1})}>Update Name</button> }
     </div>
   );
 }
